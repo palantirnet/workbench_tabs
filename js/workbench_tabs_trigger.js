366c0d387a96224fd550fc31f90285ab3c5ae0ca
Patrick Weston <patrickfweston@gmail.com>
2016-05-20 21:03:39 +0000

    Add drawer closing functionality on scroll

--- js/workbench_tabs_trigger.js
+++ js/workbench_tabs_trigger.js
@@ -5,27 +5,44 @@
 
 (function ($) {
 
-    // We're accessing this DOM element a lot, so put it in a variable.
-    $message_trigger = $('.workbench-tabs__trigger');
+  // We're accessing this DOM element a lot, so put it in a variable.
+  var $messageTrigger = $('.workbench-tabs__trigger');
+  var $messageContents = $('.workbench-tabs__message');
 
-    // Open/Close functionality for rail navigation.
-    $message_trigger.click(function(e) {
-        e.preventDefault();
+  // Open/Close functionality for rail navigation.
+  $messageTrigger.click(function(e) {
+    e.preventDefault();
 
-        // Toggle class for the drawer and also add a helper class to the
-        // children.
-        $(this).toggleClass('is-closed');
+    // Toggle class for the drawer and also add a helper class to the
+    // children.
+    $(this).toggleClass('is-closed');
 
-        $('.workbench-tabs__message').slideToggle('slow', function() {
-            $('.workbench-tabs__message').toggleClass('is-closed', $(this).is(':visible'));
-        });
+    $messageContents.slideToggle('slow', function() {
+      $messageContents.toggleClass('is-closed', $(this).is(':visible'));
     });
+  });
 
-    // Lose focus on the trigger when the mouse leaves.
-    // We could use .blur() in the click handler above, but that breaks the
-    // menu for users who are tabbing through.
-    $message_trigger.mouseout(function() {
-        $(this).blur();
-    });
+  // Lose focus on the trigger when the mouse leaves.
+  // We could use .blur() in the click handler above, but that breaks the
+  // menu for users who are tabbing through.
+  $messageTrigger.mouseout(function() {
+    $(this).blur();
+  });
+
+  // Close the drawer when we scroll past it.
+  $(window).on('scroll', function() {
+    // Get the height of the message contents.
+    var messageHeight = $messageContents.outerHeight(true);
+
+    // If we've scrolled past the messages and we are not closed, then close the drawer.
+    if ($(window).scrollTop() > messageHeight && !$messageTrigger.hasClass('is-closed')) {
+      // Reset the trigger and contents to closed.
+      $messageTrigger.addClass('is-closed');
+      $messageContents.addClass('is-closed').attr('style','');
+
+      // Scroll to the top of the page to prevent a jump.
+      $(window).scrollTop(0);
+    }
+  });
 
 })(jQuery);
